9787e6de16e9f01c07f0be695ab9cc025b383f86
ascenshain-agent <ascenshain-agent@users.noreply.github.com>
2026-03-22 16:55:27 -0400

    chore: remove .netlify directory from git tracking and ensure it is ignored

--- .netlify/functions-internal/___netlify-server-handler/.next/server/app/api/locations/[id]/route.js
+++
@@ -1,7 +0,0 @@
-var R=require("../../../../chunks/[turbopack]_runtime.js")("server/app/api/locations/[id]/route.js")
-R.c("server/chunks/[root-of-the-server]__0unoe_2._.js")
-R.c("server/chunks/node_modules_@supabase_supabase-js_dist_index_mjs_0hp37pu._.js")
-R.c("server/chunks/[root-of-the-server]__0j8-xkl._.js")
-R.c("server/chunks/_next-internal_server_app_api_locations_[id]_route_actions_0bvpotg.js")
-R.m(25112)
-module.exports=R.m(25112).exports
--- .netlify/functions-internal/___netlify-server-handler/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"L97NgGdmqhSJGJPOdbUscpr+vpNFfE+H9W6Z2VIQ3lk=\"\n}"
